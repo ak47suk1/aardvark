734777f06e31f358704b46962bfe9fd5f832bb3f
Shaun Daubney <superawesomeme@googlemail.com>
2013-01-29 15:25:23 +0000

    Update layout/embedded.php

--- layout/embedded.php
+++ layout/embedded.php
@@ -1,9 +1,9 @@
 <?php echo $OUTPUT->doctype() ?>
 <html <?php echo $OUTPUT->htmlattributes() ?>>
 <head>
-    <title><?php echo $PAGE->title ?></title>
-    <link rel="shortcut icon" href="<?php echo $OUTPUT->pix_url('favicon', 'theme')?>" />
-    <?php echo $OUTPUT->standard_head_html() ?>
+<title><?php echo $PAGE->title ?></title>
+<link rel="shortcut icon" href="<?php echo $OUTPUT->pix_url('favicon', 'theme')?>" />
+<?php echo $OUTPUT->standard_head_html() ?>
 </head>
 <body id="<?php p($PAGE->bodyid) ?>" class="<?php p($PAGE->bodyclasses) ?>">
 <?php echo $OUTPUT->standard_top_of_body_html() ?>
@@ -12,9 +12,9 @@
 
 <!-- END OF HEADER -->
 
-    <div id="content" class="clearfix">
-        <?php echo $OUTPUT->main_content() ?>
-    </div>
+<div id="content" class="clearfix">
+<?php echo $OUTPUT->main_content() ?>
+</div>
 
 <!-- START OF FOOTER -->
 </div>
