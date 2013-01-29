f26446d60865feb005d331ab1c0e96b740abb13a
Shaun Daubney <superawesomeme@googlemail.com>
2013-01-29 15:41:30 +0000

    Update config.php

--- config.php
+++ config.php
@@ -7,7 +7,6 @@ $THEME->name = 'aardvark';
 // the directory in which this file resides.
 ////////////////////////////////////////////////////
 
-
 $THEME->parents = array('base','canvas');
 
 /////////////////////////////////////////////////////
@@ -19,7 +18,6 @@ $THEME->parents = array('base','canvas');
 // extend the theme you like and just add the
 // changes you want to your theme.
 ////////////////////////////////////////////////////
-
 
 $THEME->sheets = array('core');
 
@@ -42,88 +40,88 @@ $THEME->editor_sheets = array('editor');
 ////////////////////////////////////////////////////
 
 $THEME->layouts = array(
-    'base' => array(
-        'file' => 'general.php',
-        'regions' => array('side-pre', 'side-post'),
-        'defaultregion' => 'side-pre',
-    ),
-    'standard' => array(
-        'file' => 'general.php',
-        'regions' => array('side-pre', 'side-post'),
-        'defaultregion' => 'side-pre',
-    ),
-    'course' => array(
-        'file' => 'general.php',
-        'regions' => array('side-pre', 'side-post'),
-        'defaultregion' => 'side-pre'
-    ),
-    'coursecategory' => array(
-        'file' => 'general.php',
-        'regions' => array('side-pre', 'side-post'),
-        'defaultregion' => 'side-pre',
-    ),
-	'incourse' => array(
-	'file' => 'general.php',
-	'regions' => array('side-post'),
-	'defaultregion' => 'side-post',
-    ),
-    'frontpage' => array(
-        'file' => 'general.php',
-        'regions' => array('side-pre', 'side-post'),
-        'defaultregion' => 'side-pre',
-    ),
-    'admin' => array(
-        'file' => 'general.php',
-        'regions' => array('side-pre'),
-        'defaultregion' => 'side-pre',
-    ),
-    'mydashboard' => array(
-        'file' => 'general.php',
-        'regions' => array('side-pre'),
-        'defaultregion' => 'side-pre',
-        'options' => array('langmenu'=>true),
-    ),
-    'mypublic' => array(
-        'file' => 'general.php',
-        'regions' => array('side-pre'),
-        'defaultregion' => 'side-pre',
-    ),
-    'login' => array(
-        'file' => 'general.php',
-        'regions' => array(),
-        'options' => array('langmenu'=>true),
-    ),
-    'popup' => array(
-        'file' => 'embedded.php',
-        'regions' => array(),
-        'options' => array('nofooter'=>true, 'noblocks'=>true, 'nonavbar'=>true),
-    ),
-    'frametop' => array(
-        'file' => 'general.php',
-        'regions' => array(),
-        'options' => array('nofooter'=>true),
-    ),
-    'maintenance' => array(
-        'file' => 'general.php',
-        'regions' => array(),
-        'options' => array('nofooter'=>true, 'nonavbar'=>true),
-    ),
-    'embedded' => array(
-    	'theme'=>'canvas',
-        'file' => 'embedded.php',
-        'regions' => array(),
-        'options' => array('nofooter'=>true, 'nonavbar'=>true),
-    ),
-    // Should display the content and basic headers only.
-    'print' => array(
-        'file' => 'general.php',
-        'regions' => array(),
-        'options' => array('nofooter'=>true, 'nonavbar'=>false, 'noblocks'=>true),
-    ),
-    'report' => array(
-        'file' => 'general.php',
-        'regions' => array(),
-    ),
+'base' => array(
+'file' => 'general.php',
+'regions' => array('side-pre', 'side-post'),
+'defaultregion' => 'side-pre',
+),
+'standard' => array(
+'file' => 'general.php',
+'regions' => array('side-pre', 'side-post'),
+'defaultregion' => 'side-pre',
+),
+'course' => array(
+'file' => 'general.php',
+'regions' => array('side-pre', 'side-post'),
+'defaultregion' => 'side-pre'
+),
+'coursecategory' => array(
+'file' => 'general.php',
+'regions' => array('side-pre', 'side-post'),
+'defaultregion' => 'side-pre',
+),
+'incourse' => array(
+'file' => 'general.php',
+'regions' => array('side-post'),
+'defaultregion' => 'side-post',
+),
+'frontpage' => array(
+'file' => 'general.php',
+'regions' => array('side-pre', 'side-post'),
+'defaultregion' => 'side-pre',
+),
+'admin' => array(
+'file' => 'general.php',
+'regions' => array('side-pre'),
+'defaultregion' => 'side-pre',
+),
+'mydashboard' => array(
+'file' => 'general.php',
+'regions' => array('side-pre'),
+'defaultregion' => 'side-pre',
+'options' => array('langmenu'=>true),
+),
+'mypublic' => array(
+'file' => 'general.php',
+'regions' => array('side-pre'),
+'defaultregion' => 'side-pre',
+),
+'login' => array(
+'file' => 'general.php',
+'regions' => array(),
+'options' => array('langmenu'=>true),
+),
+'popup' => array(
+'file' => 'embedded.php',
+'regions' => array(),
+'options' => array('nofooter'=>true, 'noblocks'=>true, 'nonavbar'=>true),
+),
+'frametop' => array(
+'file' => 'general.php',
+'regions' => array(),
+'options' => array('nofooter'=>true),
+),
+'maintenance' => array(
+'file' => 'general.php',
+'regions' => array(),
+'options' => array('nofooter'=>true, 'nonavbar'=>true),
+),
+'embedded' => array(
+'theme'=>'canvas',
+'file' => 'embedded.php',
+'regions' => array(),
+'options' => array('nofooter'=>true, 'nonavbar'=>true),
+),
+// Should display the content and basic headers only.
+'print' => array(
+'file' => 'general.php',
+'regions' => array(),
+'options' => array('nofooter'=>true, 'nonavbar'=>false, 'noblocks'=>true),
+),
+'report' => array(
+'file' => 'general.php',
+'regions' => array(),
+),
 );
 
 $THEME->csspostprocess = 'aardvark_process_css';
